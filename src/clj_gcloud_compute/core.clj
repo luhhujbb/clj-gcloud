6142d7625b5cde4886a1610b9b2394f77934bc3c
Jean-Baptiste Besselat <luhhujbb@aol.com>
2020-07-21 15:36:37 +0200

    [init]

---
+++ src/clj_gcloud_compute/core.clj
@@ -0,0 +1,6 @@
+(ns clj-gcloud-compute.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_gcloud_compute/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-gcloud-compute.core-test
+  (:require [clojure.test :refer :all]
+            [clj-gcloud-compute.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
